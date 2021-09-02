1c826257eda4a7b145e261e79c759631cfb2dbd7
PureYorick <52889903+PureYorick@users.noreply.github.com>
2021-09-02 12:56:37 -0500

    Update HW2Part1MethodOverloading.java

--- HW2Part1MethodOverloading.java
+++ HW2Part1MethodOverloading.java
@@ -3,7 +3,7 @@ package COMSC2;
 //import java.util.Arrays;
 import java.util.Scanner;
 
-//problem: using overloaded method -- i cannot pull the value stored in 1 method, to the other method. 
+//DESCRIPTION OF PROGRAM ON LINE 46
 // #1 (Average an array) Write two overloaded functions that return the average
 // of an array
 // Write a test program that prompts the user to enter ten values, invokes this
@@ -20,11 +20,11 @@ public class HW2Part1MethodOverloading {
 
 	public static void average() {
 		Scanner k = new Scanner(System.in);
-		int[] array = new int[10];
+		int[] array = new int[5];
 
-		System.out.println("Enter the 10 values in your array: ");
+		System.out.println("Enter the 5 values in your array: ");
 
-		for (int i = 0; i < 10; i++) { // ++ arithmetic operator
+		for (int i = 0; i < 5; i++) { // ++ arithmetic operator
 			array[i] = k.nextInt();
 		}
 		double total = 0;
@@ -34,7 +34,7 @@ public class HW2Part1MethodOverloading {
 
 		System.out.println("That totals to: " + total);
 
-		double average = total / 10;
+		double average = total / 5;
 		System.out.println("Which has an average of: " + average);
 		k.close();
 	}
@@ -43,7 +43,20 @@ public class HW2Part1MethodOverloading {
 		System.out.println();
 		//unused method attempt at overloading -- found solution online, difficult without a guide
 	}
-	
+	/* Function average is created on line 21 and called up to the main to be ran
+	 * create the scanner to request user input, labeled k for keyboard
+	 * create an array of integers titled array, parameterized by 5
+	 * loop created to store the values in the array, up to 5 values 
+	 * create new variable total set to 0 as it will be incremented in the next loop
+	 * next loop is a for each, for each index in the array, increase the total by users input 
+	 * print the total for visibility 
+	 * 
+	 * create variable average to hold the answer and print
+	 * 
+	 * changed it to 5 cause when you're testing it, 10 is redundant 
+	 * 
+	 * 
+	 */
 	
 }
 
