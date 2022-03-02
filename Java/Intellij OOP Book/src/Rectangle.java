b35cf85a1665dd6b57b8f1d8f1a4dfd553470326
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-02 13:30:12 -0600

    Edits
    Edited the example from the text to play with constructors

--- Java/Intellij OOP Book/src/Rectangle.java
+++ Java/Intellij OOP Book/src/Rectangle.java
@@ -5,9 +5,12 @@ public class Rectangle {
         length = width = 0;
     }
 
-    public Rectangle(int l, int w) { //constructor 2
+    public Rectangle(int l, int w) { 
         length = l;
         width = w;
+    }
+
+    public Rectangle(int i) {
     }
 
     public int getArea(){ //function
--- Java/Intellij OOP Book/src/RectangleDriver.java
+++ Java/Intellij OOP Book/src/RectangleDriver.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class RectangleDriver {
     public static void main(String[] args) {
        //Rectangle r  = new Rectangle();
        //r.length = 10;
        // r.width = 5;
+        Scanner k = new Scanner(System.in);
 
-        Rectangle r = new Rectangle(10,5);
-        System.out.println("The area is: " + r.getArea());
+        //Rectangle r = new Rectangle(10,5);
+        //System.out.println("The area is: " + r.getArea());
 
-        Rectangle one = new Rectangle(100, 5);
-        System.out.println("2nd rectangle area: "+ one.getArea());
+        System.out.println("Enter the length and width of a rectangle: ");
+        Rectangle user = new Rectangle(k.nextInt(), k.nextInt());
+        System.out.println("The area of the users rectangle is: " + user.getArea());
 
-        Rectangle two = new Rectangle(2155, 321);
-        System.out.println("3rd rectangle area is: "+ two.getArea());
 
+        Rectangle two = new Rectangle(100, 5);
+        System.out.println("2nd rectangle area: "+ two.getArea());
+
+        Rectangle three = new Rectangle(2155, 321);
+        System.out.println("3rd rectangle area is: "+ three.getArea());
     }
 }
 
