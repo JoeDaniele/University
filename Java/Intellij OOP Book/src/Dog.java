aee98c686082901dafe174ae2080b1f794640581
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-06 18:37:31 -0600

    Update Dog.java

--- Java/Intellij OOP Book/src/Dog.java
+++ Java/Intellij OOP Book/src/Dog.java
@@ -3,12 +3,34 @@ public class Dog {
     private int age;
     private double weight;
 
-    public Dog() {}
+    public Dog() {
+    }
 
-    public Dog(String n, String b, int a, double w){
+    public Dog(String n, String b, int a, double w) {
         name = n;
         breed = b;
         age = a;
         weight = w;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getEverything() {
+        return name + "is a " + breed + ", " + age + " years old, " + weight + " kg.";
     }
 }
