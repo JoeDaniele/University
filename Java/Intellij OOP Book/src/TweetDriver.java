225475a0782cb9dec8b0b0205f6b48e46cf14e02
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-08 13:09:58 -0600

    Continue

--- Java/Intellij OOP Book/src/TweetDriver.java
+++ Java/Intellij OOP Book/src/TweetDriver.java
@@ -1,2 +1,7 @@
 public class TweetDriver {
+    public static void main(String[] args) {
+        Tweet t = new Tweet("Tues Mar 8 1:07:00 +0000 2022", 8,
+                true, 867530999L, 2, false,
+                "You can now buy 'Learn Java The Hard Way' using bitcoin!");
+    }
 }
