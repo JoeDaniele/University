24b6092b53a8ecc76c2637bd2ac664e39b2b99d5
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-08 13:14:07 -0600

    Chapter 14

--- Java/Intellij OOP Book/src/TweetDriver.java
+++ Java/Intellij OOP Book/src/TweetDriver.java
@@ -1,7 +1,11 @@
 public class TweetDriver {
     public static void main(String[] args) {
-        Tweet t = new Tweet("Tues Mar 8 1:07:00 +0000 2022", 8,
-                true, 867530999L, 2, false,
-                "You can now buy 'Learn Java The Hard Way' using bitcoin!");
+        Tweet t = new Tweet("Tues Mar 8 1:07:00 +0000 2022",
+                8, true, 867530999L, 2, false,
+                "The author needs to learn how to use commas!");
+
+        System.out.println(t);
+        System.out.println("\n----------------------\n");
+        System.out.println(t);
     }
 }
