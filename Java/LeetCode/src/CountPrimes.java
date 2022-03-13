6d6ec199f1e217fc90cdc76201c776f656fe1179
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-13 00:09:22 -0600

    LeetCode Questions
    Reviewing solutions

---
+++ Java/LeetCode/src/CountPrimes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CountPrimes {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int u = k.nextInt();
+
+        countPrimes(u);
+    }
+
+    public static int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+}
