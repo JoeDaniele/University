316924aa746260469eba9a3b6fd019e6a9b4dfdc
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-22 10:30:34 -0500

    Update BasicStack.java

--- Java/DS&A Pluralsight Course/src/BasicStack.java
+++ Java/DS&A Pluralsight Course/src/BasicStack.java
@@ -1,3 +1,5 @@
+import org.codehaus.groovy.ast.stmt.WhileStatement;
+
 public class BasicStack<X> { //generic type of x
     private X [] data; //private scope for primitive array
     private int stackPointer; //increment or decrement based on size of stack (internal pointer on this DS)
@@ -15,8 +17,11 @@ public class BasicStack<X> { //generic type of x
         if (stackPointer == 0){
             throw new IllegalStateException("No more items in the stack");
         }
-        return data[--stackPointer]; //pre-decrement will pull the value out of the DS and return it.
-        //not actually deleting said value
+        return data[--stackPointer];
+        /*pre-decrement will pull the value out of the DS and return it.
+        not actually deleting said value
+         */
+
     }
 
     public boolean contains(X item){
@@ -35,11 +40,25 @@ public class BasicStack<X> { //generic type of x
            Once found -- set var to true and break;
            e.g. a basic linear function
          */
-
     }
 
     public X access(X item){
+        while (stackPointer > 0){
+            X tmpItem = pop();
+            if (item.equals(tmpItem)){
+                return item;
+            }
+        }
+        throw new IllegalArgumentException("Could not find the item in the stack: " + item);
+        /* While we have items on the stack -- grab each one via Pop();
+        "Destructive operation" REMOVING items off the stack as we search
+        When popped, set equal to local variable tempItem --
+        Compare to see if equal -- return.
+         */
+    }
 
-        return item;
+    public int size(){
+        return stackPointer;
+        //return current position of stack pointer and give us the size of the stack
     }
 }
