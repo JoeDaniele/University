371a29e09ec83ac021ba3eef58fdd6bb26e55d50
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-22 09:35:46 -0500

    Update BasicStack.java

--- Java/DS&A Pluralsight Course/src/BasicStack.java
+++ Java/DS&A Pluralsight Course/src/BasicStack.java
@@ -12,6 +12,34 @@ public class BasicStack<X> { //generic type of x
     }
 
     public X pop(){
-        return data[--stackPointer];
+        if (stackPointer == 0){
+            throw new IllegalStateException("No more items in the stack");
+        }
+        return data[--stackPointer]; //pre-decrement will pull the value out of the DS and return it.
+        //not actually deleting said value
+    }
+
+    public boolean contains(X item){
+        boolean found = false;
+        for (int i = 0; i <stackPointer ; i++) {
+            if (data[i].equals(item)){
+                found = true;
+                break;
+            }
+        }
+        return found;
+        /* Declare local boolean to determine whether we've found the item or not
+           Return that variable at the end of the method
+           Loop through all items in the stack
+           Comparison check on the data slot with the current item passed in
+           Once found -- set var to true and break;
+           e.g. a basic linear function
+         */
+
+    }
+
+    public X access(X item){
+
+        return item;
     }
 }
