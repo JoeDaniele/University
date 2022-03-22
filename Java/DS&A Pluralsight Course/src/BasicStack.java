3f7a5f4a0ea4492fe28143524f3516d76a7fc990
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-22 09:22:53 -0500

    Update BasicStack.java

--- Java/DS&A Pluralsight Course/src/BasicStack.java
+++ Java/DS&A Pluralsight Course/src/BasicStack.java
@@ -1,17 +1,17 @@
 public class BasicStack<X> { //generic type of x
     private X [] data; //private scope for primitive array
-    private int stackPointer;
+    private int stackPointer; //increment or decrement based on size of stack (internal pointer on this DS)
 
     public BasicStack(){ //Constructor to initialize array
         data = (X[]) new Object[1000]; //initialize data structure -- set to 1k and cast back to generic type x
-        stackPointer = 0; //increment or decrement based on size of stack (internal pointer on this DS)
+        stackPointer = 0; //initialize the stack pointer
     }
     //creating push/pop/search methods == insert/delete/search
     public void push(X newItem) {//Parameter is new item that will go on top of the stack
-
+        data[stackPointer++] = newItem;
     }
 
     public X pop(){
-
+        return data[--stackPointer];
     }
 }
