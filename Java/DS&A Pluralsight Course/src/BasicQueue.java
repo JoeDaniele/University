6ee70bdaff98bfafd28667df82b2fcaef94facba
PureYorick <52889903+PureYorick@users.noreply.github.com>
2022-03-28 13:43:35 -0500

    Continuing
    Continuing with course and got held up on modulo -- did a quick review to recall quotient vs remainder etc

---
+++ Java/DS&A Pluralsight Course/src/BasicQueue.java
@@ -0,0 +1,49 @@
+
+public class BasicQueue<T> { //create generic
+    private T[]data; //private primitive array ds
+    private int front;
+    private int end;
+
+
+    public BasicQueue(){ //calls other constructor using THIS and sets queue size to 1000
+        this(1000);
+    }
+
+    public BasicQueue(int size){ //takes size parameter/argument and initializes Q to given size
+        this.front = -1; //-1 is before 0, 0 is first in the queue
+        this.end = -1;
+        data = (T[])new Object[size];
+    }
+
+    public int size(){
+        //if the queue is empty return 0
+        if (front == -1 && end == -1) {
+            return 0; // simple check to see if the queue is empty
+        }
+        //add one to get the INCLUSIVE subtraction value rather than excluding the front
+        else{
+            return end - front + 1;
+        }
+    }
+
+    public void enQueue(T item) {
+        //check if Queue is full
+        if ((end + 1) % data.length == front){ //MODULO DIVIDES AND TAKES THE REMAINDER
+            throw new IllegalStateException("The Queue is full.");
+        }
+        //Otherwise check to see if any items have been added to the queue yet
+        else if (size() == 0){
+            front++;
+            end++;
+            data[end] = item;
+        }
+        //otherwise add the item to the end of the queue
+        else{
+            end++;
+            data[end] = item;
+        }
+    }
+    public T deQueue(){
+        return null;
+    }
+}
---
+++ Java/DS&A Pluralsight Course/src/testModuloAgain.java
@@ -0,0 +1,8 @@
+public class testModuloAgain {
+    public static void main(String[] args) {
+        int end = 1;
+        int datalength = 1000;
+
+        System.out.println((end+1) % datalength);
+    }
+}
