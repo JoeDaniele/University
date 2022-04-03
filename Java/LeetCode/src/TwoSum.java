450ddd6f43f2334823621f6b7e51b582b55f5e52
PureYorick <joefdaniele@gmail.com>
2022-04-03 12:55:59 -0500

    LEET

--- Java/LeetCode/src/TwoSum.java
+++ Java/LeetCode/src/TwoSum.java
@@ -6,7 +6,7 @@ public class TwoSum {
 
     }
 
-    public static int [] twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target){
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j <nums.length ; j++) {
                 int complement = target - nums[i];
@@ -15,8 +15,8 @@ public class TwoSum {
                     return new int[]{ i, j};
                 }
             }
-
         }
+
         throw new IllegalStateException("No pair found.");
     }
 
