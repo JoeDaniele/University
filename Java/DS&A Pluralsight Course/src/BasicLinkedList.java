9eb9c91d39228f4a445ae864576852dab5692c13
PureYorick <joefdaniele@gmail.com>
2022-04-05 15:21:02 -0500

    Continue
    Added more functions to the generic class

--- Java/DS&A Pluralsight Course/src/BasicLinkedList.java
+++ Java/DS&A Pluralsight Course/src/BasicLinkedList.java
@@ -93,6 +93,41 @@ public class BasicLinkedList<T> { //GENERIC
         return nodeItem;
     }
 
+    public int find(T item) {
+        if (first == null) {
+            throw new IllegalStateException("LinkedList is empty and there are no items to find.");
+        }
+
+        Node currentNode = first;
+        for (int i = 1; i < size() && currentNode != null; i++) {
+            if (currentNode.getNextNode().equals(item)) {
+                return i;
+            }
+        }
+        currentNode = currentNode.getNextNode();
+
+        //if we didn't find it, then return -1
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer contents = new StringBuffer();
+        Node curNode = first;
+
+        while (curNode != null) {
+            contents.append(curNode.getNextNode());
+            curNode = curNode.getNextNode();
+
+            if (curNode != null) {
+                contents.append(",");
+            }
+        }
+        return contents.toString();
+
+    }
+
+
     public int size() {
         return nodeCount;
     }
