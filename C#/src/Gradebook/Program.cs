8087de6c093f7d9c3095589fdaf7959e637dd096
PureYorick <joefdaniele@gmail.com>
2022-04-12 12:07:23 -0500

    Continuing Course

--- C#/src/Gradebook/Program.cs
+++ C#/src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System; //using system namespace == java.lang.* to remove explicit typing
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -7,11 +8,13 @@ namespace Gradebook
         static void Main(string[] args)
         {
 
-            var numbers = new[] { 12.7, 10.1, 6.11, 4.1 };
-            List grades; 
+            var numbers = new[] { 12.7, 10.1, 6.11, 4.1 }; //array of double
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
 
             var result = 0.0;
-            foreach (var number in numbers)
+            foreach (var number in grades)
             {
                 result += number;
             }
