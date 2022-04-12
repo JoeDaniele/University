c02b3baf4a5023dac18f28a1963babae2cf091a4
PureYorick <joefdaniele@gmail.com>
2022-04-12 10:37:38 -0500

    Update

--- C#/src/Gradebook/Program.cs
+++ C#/src/Gradebook/Program.cs
@@ -7,8 +7,25 @@ namespace Gradebook
         static void Main(string[] args)
         {
 
-            Console.WriteLine($"Hello {args[0]}!"); // string interpolation $
+            var numbers = new[] { 12.7, 10.1, 6.11, 4.1 };
 
+            var result = 0.0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]} {args[1]}!"); // string interpolation $
+            }
+            else
+            {
+                Console.WriteLine("Else.");
+            }
         }
     }
 }
