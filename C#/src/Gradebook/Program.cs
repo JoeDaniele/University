c6326560dd305f2faf7a8b6416d20c27e4882e1d
PureYorick <joefdaniele@gmail.com>
2022-04-13 15:27:03 -0500

    Update

--- C#/src/Gradebook/Program.cs
+++ C#/src/Gradebook/Program.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 
 namespace Gradebook
 {
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
+            Book book2 = new Book();
+            book2.AddGrade(90.1);
+
             var book = new Book();
+            book.AddGrade(89.1);
+
 
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
