351530ab3804123a813ddbf42c34ae6c45d19e95
PureYorick <joefdaniele@gmail.com>
2022-04-14 16:44:55 -0500

    big edits

--- C#/src/Gradebook/Book.cs
+++ C#/src/Gradebook/Book.cs
@@ -15,6 +15,30 @@ namespace Gradebook
         {
             this.grades.Add(grade); //on THIS object, go to the grades FIELD and add a new grade
         }
+
+        internal void ShowStats()
+        {
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                lowGrade = Math.Min(number, highGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count; //COUNT is a PROPERTY not a method. Methods require parameters
+
+            Console.WriteLine($"Total: {result:N3}"); //Formatting specifiers to limit decimals
+            Console.WriteLine($"Average of scores: {(int)result}");
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
+            Console.WriteLine($"The lowest grade is: {highGrade}");
+            Console.WriteLine($"Arraylist contains a value of 10.3? {grades.Contains(10.3)}");
+        }
+
         private List<double> grades; //instance field
         private string name; //instance field
     }
