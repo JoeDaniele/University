51f67a464aa691fd9980970b1724344dfe22e78e
PureYorick <joefdaniele@gmail.com>
2022-04-14 13:34:06 -0500

    Continued

--- C#/src/Gradebook/Book.cs
+++ C#/src/Gradebook/Book.cs
@@ -11,11 +11,11 @@ namespace Gradebook
             this.name = name; //this.  implicit variable refers to the object that is being operated on, "Book", 
         }                     //available in methods/constructors
 
-        public void AddGrade(double grade)
+        public void AddGrade(double grade) //AddGrade is an instance member of class Book
         {
             this.grades.Add(grade); //on THIS object, go to the grades FIELD and add a new grade
         }
-        private List<double> grades;
-        private string name;
+        private List<double> grades; //instance field
+        private string name; //instance field
     }
 }
--- C#/src/Gradebook/Program.cs
+++ C#/src/Gradebook/Program.cs
@@ -10,22 +10,31 @@ namespace Gradebook
             var book = new Book("Master Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.grades.Add(101);
+            book.AddGrade(77.5); //B = class name : b = variable name
 
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
             foreach (var number in grades)
             {
+                lowGrade = Math.Min(number, highGrade);
+                highGrade = Math.Max(number, highGrade);
                 result += number;
             }
             result /= grades.Count; //COUNT is a PROPERTY not a method. Methods require parameters
 
             Console.WriteLine($"Total: {result:N3}"); //Formatting specifiers to limit decimals
             Console.WriteLine($"Average of scores: {(int)result}");
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
+            Console.WriteLine($"The lowest grade is: {highGrade}");
             Console.WriteLine($"Arraylist contains a value of 10.3? {grades.Contains(10.3)}");
         }
+        public void showStats()
+        {
 
+        }
     }
 }
