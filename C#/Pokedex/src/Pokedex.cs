cbb4874c390dcbad5d200e111489fec98a502cda
PureYorick <joefdaniele@gmail.com>
2022-04-15 11:59:34 -0500

    Project
    Step one complete

---
+++ C#/Pokedex/src/Pokedex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace src
+{
+    class Pokedex
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("oi");
+
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("Starters.txt"))
+            {
+                // Read the stream as a string, and write the string to the console.
+                Console.WriteLine(sr.ReadToEnd());
+            }
+
+
+
+
+        }
+    }
+}
