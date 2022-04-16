fc1e5495668bcec9377d468b3bcbb41c15ace3f7
PureYorick <joefdaniele@gmail.com>
2022-04-16 11:30:48 -0500

    Update
    cleaned up

--- C#/src/Gradebook/Program.cs
+++ C#/src/Gradebook/Program.cs
@@ -13,10 +13,6 @@ namespace Gradebook
             book.AddGrade(77.5); //B = class name : b = variable name
             book.ShowStats();
 
-
-            int a = 2357;
-            int b = 1738;
-            System.Console.WriteLine(a + b);
         }
     }
 }
