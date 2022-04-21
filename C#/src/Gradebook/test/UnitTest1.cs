39768f5bd57a925fff1643e8c3705b57c3c70717
PureYorick <joefdaniele@gmail.com>
2022-04-21 11:05:00 -0500

    Update UnitTest1.cs

--- C#/src/Gradebook/test/UnitTest1.cs
+++ C#/src/Gradebook/test/UnitTest1.cs
@@ -10,12 +10,14 @@ public class BookTests
     {
         //arrange
         var book = new Book("");
-
+        book.AddGrade(89.1);
+        book.AddGrade(90.5);
+        book.AddGrade(77.3);
         //act
-
+        var result = book.ShowStats();
 
         //assert
-
+        Assert.Equal(85.6, result.Average);
 
 
     }
