8afefc3ead7e4cba7f6033c96f7d64c9ea107208
PureYorick <joefdaniele@gmail.com>
2022-04-21 10:22:36 -0500

    Unit Testing

---
+++ C#/src/Gradebook/test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace Gradebook.Test;
+
+public class BookTests
+{
+    [Fact]
+    public void Test1()
+    {
+        //arrange
+        var book = new Book();
+
+        //act
+        var actual = x + y;
+
+        //assert
+        Assert.Equal(expected, actual);
+
+
+
+    }
+}
