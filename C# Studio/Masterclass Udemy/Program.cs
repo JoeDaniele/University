dca69e2141f37b9906bda12bae4dae113d865f21
PureYorick <joefdaniele@gmail.com>
2022-05-04 04:45:52 -0500

    Easy task

---
+++ C# Studio/Masterclass Udemy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Masterclass_Udemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Lennox Lewis", "Joe Daniele", "Manny Pacquiao", "Roberto Duran", "Ray Leonard" };
+
+            foreach (string name in names)
+            {
+                Console.WriteLine($"{name}! Welcome to the program.");
+            }
+            Console.ReadKey();
+
+            /* Creating arraylist of names followed by a foreach loop
+             * for each loop cycles thru the names
+             * prints each each name using string interpolation 
+             */
+
+        }
+    }
+}
