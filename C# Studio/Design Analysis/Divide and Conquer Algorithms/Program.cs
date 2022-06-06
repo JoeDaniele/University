04494b0bbcb4b0473d5a7ee62a35d6858444f4db
PureYorick <joefdaniele@gmail.com>
2022-06-06 15:39:24 -0500

    Working on timing execution of file

--- C# Studio/Design Analysis/Divide and Conquer Algorithms/Program.cs
+++ C# Studio/Design Analysis/Divide and Conquer Algorithms/Program.cs
@@ -4,9 +4,13 @@
  * Research memory requirements, 
  * time complexitry,
  * algorithm complexity (difficulty). 
+ * 
  * Create a fixture to use both algorithms on the same data set multiple times. 
+ * 
  * Time how long it takes for each algorithm to complete the sorting task on average.
+ * 
  * Please use an input set large enough to provide data suggesting a clear winner. 
+ * 
  * Make a video where you explain the two algorithms, share your research, and present your data.
  */
 
@@ -16,7 +20,16 @@ namespace Divide_and_Conquer_Algorithms
     {
         static void Main(string[] args)
         {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
 
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("HOW LONG DOES THIS TAKE TO RUN?");
+            }
+
+            stopwatch.Stop();
+            System.Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds}ms");
         }
     }
 }
