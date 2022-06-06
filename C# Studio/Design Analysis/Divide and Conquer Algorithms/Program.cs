d6ef3dcb8e2a02555e7e83bcbe7acddfc855714e
PureYorick <joefdaniele@gmail.com>
2022-06-06 14:44:21 -0500

    Assignment Update

--- C# Studio/Design Analysis/Divide and Conquer Algorithms/Program.cs
+++ C# Studio/Design Analysis/Divide and Conquer Algorithms/Program.cs
@@ -5,7 +5,8 @@
  * time complexitry,
  * algorithm complexity (difficulty). 
  * Create a fixture to use both algorithms on the same data set multiple times. 
- * Time how long it takes for each algorithm to complete the sorting task on average. Please use an input set large enough to provide data suggesting a clear winner. 
+ * Time how long it takes for each algorithm to complete the sorting task on average.
+ * Please use an input set large enough to provide data suggesting a clear winner. 
  * Make a video where you explain the two algorithms, share your research, and present your data.
  */
 
