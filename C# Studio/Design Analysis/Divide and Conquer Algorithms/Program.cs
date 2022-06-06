28961fea9ca2a2b2c03d63428ca3e69d46829673
PureYorick <joefdaniele@gmail.com>
2022-06-06 14:29:09 -0500

    Algo Class

---
+++ C# Studio/Design Analysis/Divide and Conquer Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Mergesort VS Quicksort performance in relation to big o.
+
+ * Research memory requirements, 
+ * time complexitry,
+ * algorithm complexity (difficulty). 
+ * Create a fixture to use both algorithms on the same data set multiple times. 
+ * Time how long it takes for each algorithm to complete the sorting task on average. Please use an input set large enough to provide data suggesting a clear winner. 
+ * Make a video where you explain the two algorithms, share your research, and present your data.
+ */
+
+namespace Divide_and_Conquer_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
