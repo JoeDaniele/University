70aad11ab9bbf74fe69619efea7f63505f029470
PureYorick <joefdaniele@gmail.com>
2022-06-20 21:35:26 -0500

    Updates
    Playing around with arrays

--- C# Studio/TemplateTestProject/Program.cs
+++ C# Studio/TemplateTestProject/Program.cs
@@ -10,6 +10,33 @@ namespace TemplateTestProject
     {
         static void Main(string[] args)
         {
+
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+
+
+            int[] arrayTwo = { 0, 1, 2, 3 };
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < arrayTwo.Length; i++)
+            {
+                Console.WriteLine(arrayTwo[i]);
+            }
+
+            Console.WriteLine($"Printing element in arrayTwo at index 3:   {arrayTwo[3]}");
+
+            float[] array = new float { 3, 5, -4, 8, 11, 1, -1, 6 };
+
+
+            List<int> arrayList = new List<int>();
+
+
+
+
         }
     }
 }
