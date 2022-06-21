957de5656e50167878ee0929d96a5c617489d0ef
PureYorick <joefdaniele@gmail.com>
2022-06-21 11:59:02 -0500

    Continue Class

--- C# Studio/Masterclass/Masterclass/Program.cs
+++ C# Studio/Masterclass/Masterclass/Program.cs
@@ -10,7 +10,27 @@ namespace Masterclass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\n");
+
+            int lengthoftext = 0;
+            int counter = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Enter the name of a friend: ");
+                string name = Console.ReadLine();
+                int currentLength = name.Length;
+                lengthoftext += currentLength;
+                wholeText += name;
+            } while (lengthoftext < 10);
+            Console.WriteLine("Thats all folks!" + wholeText);
+            Console.Read();
+
         }
     }
 }
