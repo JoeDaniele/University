bc72364ee3fc0687b89993b6932cd3d5e4d55c9f
PureYorick <joefdaniele@gmail.com>
2022-06-22 12:15:11 -0500

    Continue

---
+++ C# Studio/Masterclass/Masterclass/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+public class CourseChallenges
+{
+    public CourseChallenges()
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+
+    }
+}
---
+++ C# Studio/Masterclass/Masterclass/ClassExercises.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Masterclass
+{
+    internal class CourseExercises
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            RunBreakContSolutionTwo();
+
+
+        }
+        public static void ForLoop()
+        {
+            for (int i = -3; i <= 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void WhileLoop()
+        {
+            int counter = -3;
+            while (counter <= 3)
+            {
+                Console.WriteLine(counter);
+                counter++;
+                if (counter == 4)
+                {
+                    break;
+                }
+
+            }
+
+            int countTwo = 0;
+            string userInput = "";
+            while (userInput.Equals(""))
+            {
+                Console.WriteLine("Press enter to increase amt by 1: ");
+                userInput = Console.ReadLine();
+                countTwo++;
+                Console.WriteLine($"Current count is {countTwo}");
+            }
+        }
+        public static void RunForWhile()
+        {
+            WhileLoop();
+            Console.WriteLine("\n");
+            ForLoop();
+
+        }
+        public static void RunBreakCont()
+        {
+
+            for (int i = -10; i <= 10; i++)
+
+                while (i % 3 == 0)
+                {
+                    Console.WriteLine("Skip");
+                    // TODO
+
+                    Console.WriteLine(i++);
+                }
+        }
+        public static void RunBreakContSolutionTwo()
+        {
+            int i = -10;
+
+            while (true)
+            {
+                if (i % 3 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                if (i == 10)
+                    break;
+                Console.WriteLine(i++);
+
+            }
+        }
+    }
+}
+
+
