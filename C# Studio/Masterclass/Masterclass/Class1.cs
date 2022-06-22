0338bca6e255480e359b3a6e9e057f70169272a0
PureYorick <joefdaniele@gmail.com>
2022-06-22 13:43:57 -0500

    Continue

--- C# Studio/Masterclass/Masterclass/Class1.cs
+++ C# Studio/Masterclass/Masterclass/Class1.cs
@@ -10,7 +10,7 @@ public class CourseChallenges
         static void Main(string[] args)
         {
 
-
+            Console.WriteLine();
 
 
 
--- C# Studio/Masterclass/Masterclass/ClassExercises.cs
+++ C# Studio/Masterclass/Masterclass/ClassExercises.cs
@@ -10,10 +10,7 @@ namespace Masterclass
     {
         static void Main(string[] args)
         {
-
-
-
-            RunBreakContSolutionTwo();
+            Average();
 
 
         }
@@ -85,7 +82,66 @@ namespace Masterclass
 
             }
         }
+
+        public static void Average()
+        {
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
+
+            while (input != "-1")
+            {
+                Console.WriteLine($"Last number was: {currentNumber}");
+                Console.WriteLine("Enter the next score:");
+                Console.WriteLine($"Number entries thus far {count}");
+                Console.WriteLine("Enter -1 to calculate the average.");
+
+                input = Console.ReadLine();
+                if (input == "-1")
+                {
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine($"The average is: {(double)total / count}");
+                }
+                if (int.TryParse(input, out currentNumber)
+                    && currentNumber > 0 && currentNumber < 101)
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if (!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 100.");
+                    }
+                    continue;
+                }
+
+                count++;
+            }
+        }
+    }
+
+    public static void MyAverage()
+    {
+
     }
 }
 
 
+/* avg score for students
+ * enter scores
+ * final average
+ * loop for student scores
+ * add it to the sum
+ * 
+ * 
+ * get scores
+ * get average
+ * 
+ * THEN
+ * make sure numbers are between 0 and 20
+ * 
+ * 
+ * 
+ */
