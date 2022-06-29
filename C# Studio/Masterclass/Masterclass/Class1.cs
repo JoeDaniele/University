6ccffd16bf0a33ea7ca1f2f89fc3e203e7c273ca
PureYorick <joefdaniele@gmail.com>
2022-06-29 11:25:30 -0500

    Continuing course

---
+++ C# Studio/Masterclass/Masterclass/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masterclass
+{
+    class Human
+    {
+        //member variable
+        string firstName;
+
+
+    }
+}
