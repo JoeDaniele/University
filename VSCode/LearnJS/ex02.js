c648c4ca77096b8a2eef4de95e42e7a076e8f333
PureYorick <joefdaniele@gmail.com>
2022-07-18 15:20:00 -0500

    Starting JS

---
+++ VSCode/LearnJS/ex02.js
@@ -0,0 +1,8 @@
+//comments are done the same way luckily I was worried about that
+/* this is better than bad html comments e.g. !<---> or whatever
+ */
+
+console.log("I am code get at me ")
+
+
+console.log("coding coding coding ezmode ")
---
+++ VSCode/LearnJS/ex03.js
@@ -0,0 +1,7 @@
+//simple computation exercise it seems
+console.log("Name:", 'Lennox Claudius Lewis')
+console.log("Age:", 28)
+console.log("Feet Tall:", Math.round(72 / 12)) // use capital Math.FUNCTIONS like JAVA & C#
+console.log("And Inches:", 74 - (Math.round(72 / 12) * 12))
+console.log("Age * Height:", 28 * 72)
+    //etc etc etc
