f72742610cc7b2db087d013bbb1ce59018fd6b71
PureYorick <joefdaniele@gmail.com>
2022-07-20 17:24:48 -0500

    Update
    I'll take this seriously and grind it out soon

--- VSCode/LearnJS/ex04.js
+++ VSCode/LearnJS/ex04.js
@@ -1,10 +1,15 @@
 const invincible = 94;
 let changeme = 82;
 
-console.log(`invincible=${invincible} but changeme=${changeme}`);
+console.log(`invincible = ${invincible}, but changeme = ${changeme}`);
 //looks like were printing the variables like 
 //string interpolation style or something
 
 changeme = 100;
 
-//
+//uncomment this to see how you cannot do it
+//thats implied because its const not let
+//invincible = 1000;
+
+
+console.log(`After change: invincible = ${invincible}, but changeme = ${changeme}`)
---
+++ VSCode/LearnJS/ex05.js
@@ -0,0 +1,11 @@
+let talking = "I'm not sure if she said, \"Hello\" or not."
+let walking = "I\'ll need to go \'cause you don\'t know."
+
+//these string are broken fix em fool
+
+//So im not writing bad code then fixing it
+//im going to write it correctly from jump. 
+let height = "6'2";
+console.log(walking, "\n" + talking)
+console.log(height)
+console.log("I'm not going to see all of you for a while lol?")
